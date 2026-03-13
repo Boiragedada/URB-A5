e5bf8cee6e5f9dd98ebf6797d40d0653a1a2145b
Boiragedada <boiragegaming@gmail.com>
2026-03-13 21:01:56 +0600

    compleate login js

---
+++ login.js
@@ -0,0 +1,11 @@
+function login(){
+    const username = document.getElementById("username").value 
+    const password = document.getElementById("password").value 
+
+    if(username === "admin" & password ==="admin123"){
+        window.location.href ="index.html"
+    }
+    else{
+        alart("invalid password")
+    }
+}
